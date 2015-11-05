69ab52d42b5864b2304126aeed5d1885d04a5417
alyssa_quek <alyssaq@apple.com>
2015-11-05 12:30:30 +0800

    Added history to routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
 import React from 'react' // eslint-disable-line
-import { Route, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+import { createHistory } from 'history'
 import App from './containers/App'
 import NutrientPage from './containers/NutrientPage'
 import AboutPage from './containers/AboutPage'
 import NotFoundPage from './containers/NotFoundPage'
 
+export const history = createHistory()
 export default (
-  <Route path='/' component={App}>
-    <IndexRoute component={NutrientPage}/>
-    <Route path='nutrients' component={NutrientPage} />
-    <Route path='about' component={AboutPage} />
-    <Route path='*' component={NotFoundPage}/>
-  </Route>
+  <Router history={history}>
+    <Route path='/' component={App}>
+      <IndexRoute component={NutrientPage}/>
+      <Route path='nutrients' component={NutrientPage} />
+      <Route path='about' component={AboutPage} />
+      <Route path='*' component={NotFoundPage}/>
+    </Route>
+  </Router>
 )
