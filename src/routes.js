f8a310cd0ff17c45f850bf6ec28d5ee87bfbbefc
alyssa_quek <alyssaquek@gmail.com>
2015-11-05 12:59:13 +0800

    Allow different base route to app

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,15 @@
 import React from 'react' // eslint-disable-line
-import { Router, Route, IndexRoute } from 'react-router'
-import { createHistory } from 'history'
+import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import NutrientPage from './containers/NutrientPage'
 import AboutPage from './containers/AboutPage'
 import NotFoundPage from './containers/NotFoundPage'
 
-export const history = createHistory()
 export default (
-  <Router history={history}>
-    <Route path='/' component={App}>
-      <IndexRoute component={NutrientPage}/>
-      <Route path='nutrients' component={NutrientPage} />
-      <Route path='about' component={AboutPage} />
-      <Route path='*' component={NotFoundPage}/>
-    </Route>
-  </Router>
+  <Route path='/' component={App}>
+    <IndexRoute component={NutrientPage}/>
+    <Route path='nutrients' component={NutrientPage} />
+    <Route path='about' component={AboutPage} />
+    <Route path='*' component={NotFoundPage}/>
+  </Route>
 )
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { reduxReactRouter } from 'redux-router'
 import thunkMiddleware from 'redux-thunk'
-import createHistory from 'history/lib/createBrowserHistory'
+import { createHistory, useBasename } from 'history'
 import routes from '../routes'
 import rootReducer from '../reducers'
 
+const baseTag = document.getElementsByTagName('base')[0]
+const baseRoute = baseTag ? baseTag.getAttribute('href') : '/'
+const createHistoryWithBasename = (historyOptions) => {
+  return useBasename(createHistory)({
+    basename: baseRoute,
+    ...historyOptions
+  })
+}
+
 const finalCreateStore = compose(
   applyMiddleware(thunkMiddleware),
-  reduxReactRouter({ routes, createHistory })
+  reduxReactRouter({
+    routes,
+    createHistory: createHistoryWithBasename
+  })
 )(createStore)
 
 export default function configureStore (initialState) {
