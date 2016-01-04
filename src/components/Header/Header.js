974150a68eb160ab806428d4778709f02ff12ae4
alyssaq <alyssaquek@gmail.com>
2016-01-04 13:58:27 +0800

    Convert header and Navigation to stateless components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,13 @@ import './Header.styl'
 import { IndexLink } from 'react-router'
 import Navigation from './Navigation'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <header>
-        <strong>
-          <IndexLink to='/'> React Redux Example </IndexLink>
-        </strong>
-        <Navigation />
-      </header>
-    )
-  }
+export default (props) => {
+  return (
+    <header>
+      <strong>
+        <IndexLink to='/'> React Redux Example </IndexLink>
+      </strong>
+      <Navigation />
+    </header>
+  )
 }
-
-export default Header
--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -2,22 +2,17 @@ import React from 'react'
 import { Link } from 'react-router'
 import './Navigation.styl'
 
-class Navigation extends React.Component {
-
-  render () {
-    return (
-      <nav role='navigation'>
-        <ul>
-          <li>
-            <Link to='/nutrients'>Nutrients</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
+export default (props) => {
+  return (
+    <nav role='navigation'>
+      <ul>
+        <li>
+          <Link to='/nutrients'>Nutrients</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
-
-export default Navigation
