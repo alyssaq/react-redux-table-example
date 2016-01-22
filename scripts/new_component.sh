ca691137eee4d1580f2206be3b55ee668846e4f5
alyssaq <alyssaquek@gmail.com>
2016-01-22 09:53:56 -0800

    -a flag to preserve structure and attributes of files

--- scripts/new_component.sh
+++ scripts/new_component.sh
@@ -8,7 +8,7 @@ old=$1
 new=$2
 
 echo "Creating $component_folder/$new"
-cp -r $component_folder/$1/ $component_folder/$new/
+cp -Ra $component_folder/$1/ $component_folder/$new/
 mv $component_folder/$new/$old.js  $component_folder/$new/$new.js
 mv $component_folder/$new/$old.styl  $component_folder/$new/$new.styl
 find $component_folder/$new/ -type f -exec sed -i '' s/$old/$new/g {} +
