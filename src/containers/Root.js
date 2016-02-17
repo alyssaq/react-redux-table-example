1fd3d930be35ab724217d32879aacaefbf98fbe9
alyssaq <alyssaquek@gmail.com>
2016-02-17 13:59:22 +0800

    Upgrade react-router to 2.0.0. Removed history dependency

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import routes from '../routes'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-const history = createBrowserHistory()
 
 export default class Root extends React.Component {
   render () {
     const { store } = this.props
     return (
       <Provider store={store}>
-        <Router history={history}>{routes}</Router>
+        <Router history={browserHistory}>{routes}</Router>
       </Provider>
     )
   }
