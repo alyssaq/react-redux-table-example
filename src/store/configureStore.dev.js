79718c8bf9cdaa54980ce4d210f5123e4d45d9f9
alyssaq <alyssaquek@gmail.com>
2017-06-24 16:28:37 +0800

    Update and moved redux-logger to dev dependencies

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,20 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import logger from 'redux-logger'
 import rootReducer from '../reducers'
-const logger = createLogger()
 
 const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore)
 export default function configureStore (initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState)
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers')
-      store.replaceReducer(nextRootReducer)
-    })
-  }
-
-  return store
+  return createStoreWithMiddleware(rootReducer, initialState)
 }
